6903a7796acedd85ae86cb956ac0e4621152e8e9
Marlena Broniewicz <bronka1.dev@gmail.com>
2025-10-03 10:37:45 +0200

    Created nav

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,10 +1,20 @@
-import UserAuth from "@/components/UserAuth";
+import UserAuth from "@/components/Auth/UserAuth";
+import Logo from "@/components/icons/Logo";
+import Link from "next/link";
 import React from "react";
 
 const AuthPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <UserAuth />
+    <div>
+      <div className="container mx-auto flex justify-between items-center h-[50px] ">
+        <Logo />
+        <nav>
+          <Link href="/">Home</Link>
+        </nav>
+      </div>
+      <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <UserAuth />
+      </div>
     </div>
   );
 };
