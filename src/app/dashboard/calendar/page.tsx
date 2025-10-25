1e842ec14aaf7d81436ea0da2e1537b475cf3667
Marlena Broniewicz <bronka1.dev@gmail.com>
2025-10-25 20:13:53 +0200

    Calendar Page UI

---
+++ src/app/dashboard/calendar/page.tsx
@@ -0,0 +1,13 @@
+import BigCalendar from "@/components/UI/BigCalendar";
+import SectionContainer from "@/components/UI/SectionContainer";
+import React from "react";
+
+const CalendarPage = () => {
+  return (
+    <SectionContainer>
+      <BigCalendar />
+    </SectionContainer>
+  );
+};
+
+export default CalendarPage;
