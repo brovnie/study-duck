8b4bd855fb1fecab9575fc7a44996cb799c16b6c
Marlena Broniewicz <bronka1.dev@gmail.com>
2025-10-26 16:37:23 +0100

    Fixed spacing

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,11 +1,10 @@
 import DashboardCalendar from "@/components/Dashboard/Dashboard/Calendar";
-import Chart from "@/components/Dashboard/Dashboard/Chart";
 import Stats from "@/components/Dashboard/Dashboard/Stats";
 import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className="h-full w-full flex flex-col-reverse items-center xl:flex-row px-5 gap-5">
+    <div className="h-full w-full flex flex-col-reverse items-center xl:items-start xl:flex-row px-5 gap-5">
       <Stats />
       <DashboardCalendar />
     </div>
