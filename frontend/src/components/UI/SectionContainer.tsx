b6f08adc5626a902adf51e90cedac600b51628be
Marlena Broniewicz <bronka1.dev@gmail.com>
2026-02-18 21:10:16 +0100

    Added margin top and bottom

--- frontend/src/components/UI/SectionContainer.tsx
+++ frontend/src/components/UI/SectionContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SectionContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="w-full px-5 mt-5">
+    <section className="w-full px-5 my-5">
       <div className="flex flex-col bg-white rounded-md px-3 py-2">
         {children}
       </div>
