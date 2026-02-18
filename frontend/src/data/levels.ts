7552679189afdc70babcaf54d6e8a81fd2d7d8c2
Marlena Broniewicz <bronka1.dev@gmail.com>
2026-02-18 21:16:58 +0100

    Created data for levels

---
+++ frontend/src/data/levels.ts
@@ -0,0 +1,37 @@
+export const levels = [
+  {
+    level: "gray",
+    points: 0,
+    color: "fill-gray-600",
+  },
+  {
+    level: "green",
+    points: 50,
+    color: "fill-green-600",
+  },
+  {
+    level: "yellow",
+    points: 100,
+    color: "fill-yellow-600",
+  },
+  {
+    level: "orange",
+    points: 150,
+    color: "fill-orange-600",
+  },
+  {
+    level: "red",
+    points: 200,
+    color: "fill-red-600",
+  },
+  {
+    level: "blue",
+    points: 250,
+    color: "fill-blue-600",
+  },
+  {
+    level: "purple",
+    points: 300,
+    color: "fill-fuchsia-600",
+  },
+];
