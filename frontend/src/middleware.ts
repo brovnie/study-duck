d609b29113f4d4750d526526c045fd4e631d9140
Marlena Broniewicz <bronka1.dev@gmail.com>
2026-02-23 21:31:00 +0100

    Clean up unused code

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,21 +1,12 @@
-import { jwtDecode } from "jwt-decode";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { getUserById } from "./lib/api/users";
-
-interface JWTTokensInterface {
-  id: string;
-  iat: number;
-  exp: number;
-}
 
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get("jwt")?.value;
 
   const url = "/auth?signup=false";
   if (!token) {
-    console.log("No token found, redirecting to auth page");
-    //return NextResponse.redirect(new URL(url, req.url));
+    return NextResponse.redirect(new URL(url, req.url));
   }
 
   return NextResponse.next();
