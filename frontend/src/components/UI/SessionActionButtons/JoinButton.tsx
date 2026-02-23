a718e845d3ad27d4bb7b1319739bdac88fb21007
Marlena Broniewicz <bronka1.dev@gmail.com>
2026-02-23 21:27:26 +0100

    clean up code

--- frontend/src/components/UI/SessionActionButtons/JoinButton.tsx
+++ frontend/src/components/UI/SessionActionButtons/JoinButton.tsx
@@ -5,26 +5,14 @@ import { useUser } from "@/context/UserContext";
 import { useJoinSession } from "@/hooks/mutations/useJoinSession";
 import { useQueryClient } from "@tanstack/react-query";
 
-const JoinButton = ({
-  sessionId,
-  sessionType,
-}: {
-  sessionId: string | undefined;
-  sessionType: string;
-}) => {
+const JoinButton = ({ sessionId }: { sessionId: string | undefined }) => {
   const joinSession = useJoinSession();
   const { user } = useUser();
-  const queryClient = useQueryClient();
-  const queries = ["user", "sessions", "planned-available", sessionType];
 
   const handleJoin = async () => {
-    /* await joinSession.mutate({
+    await joinSession.mutate({
       id: sessionId,
       userId: user?.id,
-    });*/
-    queryClient.refetchQueries({
-      queryKey: queries,
-      exact: true,
     });
   };
   return (
